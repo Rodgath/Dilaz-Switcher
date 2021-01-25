aa0e8f3557430522a8c543216702670cc0ed2316
Rodgath <rodgath@gmail.com>
2021-01-25 08:38:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+/**
+ * Run event after DOM is ready
+ * @param {Function} fn Callback function
+ */
+var DOMready = function(fn) {
+
+	/* Sanity check */
+	if (typeof fn !== 'function') return;
+
+	/* If document is already loaded, run method */
+	if (document.readyState === 'interactive' || document.readyState === 'complete') {
+		return fn();
+	}
+
+	/* Otherwise, wait until document is loaded */
+	document.addEventListener('DOMContentLoaded', fn, false);
+
+};
+
+/* DOM is ready */
+DOMready(function() {
+	
+});
